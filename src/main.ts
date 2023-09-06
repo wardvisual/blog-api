920e40f38897d9ce76169ba92b2392a2220e6094
wardvisual <wardvisual@gmail.com>
2023-09-07 02:19:17 +0800

    chore: add HTTPFilter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+
+import { HttpExceptionFilter } from '@/lib/filters/http-exception/http-exception.filter';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
