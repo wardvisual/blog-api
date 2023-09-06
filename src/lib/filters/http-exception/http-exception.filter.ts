689d285dae476fa04f31068213bed3632c557adc
wardvisual <wardvisual@gmail.com>
2023-09-07 02:19:04 +0800

    chore: add HTTPFilter

---
+++ src/lib/filters/http-exception/http-exception.filter.ts
@@ -0,0 +1,56 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger: Logger;
+
+  constructor() {
+    this.logger = new Logger();
+  }
+
+  catch(exception: Error, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const statusCode =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.message
+        : 'Internal Server Error';
+
+    const devErrorResponse: any = {
+      isSuccess: false,
+      statusCode,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      error: exception?.name,
+      message: exception['response'].message,
+    };
+
+    const prodErrorResponse: any = {
+      isSuccess: false,
+      statusCode,
+      message,
+    };
+
+    this.logger.log(
+      `\nRequest Method: ${request.method}\nRequestUrl: ${request.url
+      }\n${JSON.stringify(devErrorResponse)}}\n`
+    );
+
+    response
+      .status(statusCode)
+      .json(
+        process.env.NODE_ENV === 'development'
+          ? devErrorResponse
+          : prodErrorResponse
+      );
+  }
+}
