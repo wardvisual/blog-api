df924fec813191c3ce7a8d69bc48ea02168bc4f1
wardvisual <wardvisual@gmail.com>
2023-09-06 19:45:54 +0800

    feat: add multer config

---
+++ src/lib/config/multer.config.ts
@@ -0,0 +1,26 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+import { diskStorage } from "multer";
+import * as fs from 'fs'
+import * as path from "path";
+
+export const multerOptions = {
+    fileFilter: (req: any, file: any, cb: any) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+            cb(null, true);
+        } else {
+            cb(new HttpException(`Unsupported file type ${path.extname(file.originalname)}`, HttpStatus.BAD_REQUEST), false);
+        }
+    },
+    storage: diskStorage({
+        destination: (req: any, file: any, cb: any) => {
+            const uploadPath = 'uploads';
+            if (!fs.existsSync(uploadPath)) {
+                fs.mkdirSync(uploadPath);
+            }
+            cb(null, uploadPath);
+        },
+        filename: (req: any, file: any, cb: any) => {
+            cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+        },
+    }),
+};
