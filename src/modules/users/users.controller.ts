4366fb3daa54110bda6631f0e527351bf1a0da52
wardvisual <wardvisual@gmail.com>
2023-09-07 03:13:03 +0800

    chore: use base-controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,37 +6,50 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { BaseController } from '@/lib/controllers/base.controller';
+
+
 @Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+export class UsersController extends BaseController {
+  constructor(private readonly usersService: UsersService) {
+    super();
+  }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body() user: CreateUserDto, @Res() response: Response) {
+    const result = this.usersService.create(user)
+    return this.responseHandler(result, response);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Res() response: Response) {
+    const result = this.usersService.findAll();
+    return this.responseHandler(result, response);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  findOne(@Param('id') id: string, @Res() response: Response) {
+    const result = this.usersService.findOne(id);
+    return this.responseHandler(result, response);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto, @Res() response: Response) {
+    const result = this.usersService.update(id, updateUserDto);
+    return this.responseHandler(result, response);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  remove(@Param('id') id: string, @Res() response: Response) {
+    const result = this.usersService.remove(id);
+    return this.responseHandler(result, response);
   }
 }
