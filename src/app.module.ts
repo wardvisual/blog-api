c5754ca87a385e6d65a61ef6d8246772e58848ca
wardvisual <wardvisual@gmail.com>
2023-09-08 20:52:01 +0800

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ResourceModule } from '@/modules/resource.module';
 import { dataSourceOptions } from '@/lib/config/typeorm.config';
+import { AuthModule } from '@/lib/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(dataSourceOptions),
+    AuthModule,
     ResourceModule,
   ],
   providers: [],
