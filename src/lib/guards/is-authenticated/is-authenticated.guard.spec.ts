c515e1cff0c269c9c7c2daf443505503fa21887c
wardvisual <wardvisual@gmail.com>
2023-09-09 00:49:59 +0800

    chore: move auth guard directory

--- src/lib/guards/is-authenticated/is-authenticated.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IsAuthenticatedGuard } from './is-authenticated.guard';
-
-describe('IsAuthenticatedGuard', () => {
-  it('should be defined', () => {
-    expect(new IsAuthenticatedGuard()).toBeDefined();
-  });
-});
