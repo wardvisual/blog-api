c9612668a6de4b19d3bc23443536dddff6e4bbb2
wardvisual <wardvisual@gmail.com>
2023-09-09 13:55:42 +0800

    ref: configure app port

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
 
+import { configService } from '@/lib/services/env.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,7 +12,7 @@ async function bootstrap() {
 
   app.use('/public/assets', express.static('public'));
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('APP_PORT'));
 }
 
 bootstrap();
