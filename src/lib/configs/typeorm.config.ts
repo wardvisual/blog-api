e4772e0feeadbb4d36fb93fb4231c961a1edbcab
wardvisual <wardvisual@gmail.com>
2023-09-14 23:52:33 +0800

    feat: updatetypeorm config

--- src/lib/configs/typeorm.config.ts
+++ src/lib/configs/typeorm.config.ts
@@ -1,24 +1,24 @@
-import { ResourceModule } from '@/modules/resource.module';
-import { ConfigService } from '@nestjs/config';
-import {
-  TypeOrmModuleAsyncOptions,
-  TypeOrmModuleOptions,
-} from '@nestjs/typeorm';
-import { DatabaseType } from 'typeorm';
+import { DatabaseType, DataSource, DataSourceOptions } from 'typeorm';
 
-export const dataSourceConfig: TypeOrmModuleOptions = {
-  imports: [ResourceModule],
-  useFactory: async (configService: ConfigService) => ({
-    type: configService.get<string>('DATABASE_DRIVER') as DatabaseType,
-    port: configService.get<number>('DATABASE_PORT'),
-    username: configService.get<string>('DATABASE_USERNAME'),
-    password: configService.get<string>('DATABASE_PASSWORD'),
-    database: configService.get<string>('DATABASE_NAME'),
-    // entities: ['src/modules/**/*.entity{.ts,.js}'],
-    // migrations: ['src/lib/database/migrations/**/*{.ts,.js}'],
-    autoLoadEntities: true,
-    synchronize: true,
-    migrationRun: false,
-  }),
-  inject: [ConfigService],
-} as TypeOrmModuleAsyncOptions;
+import { configService } from '@/lib/services/env.service';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const config: any = {
+  type: configService.get<string>('DATABASE_DRIVER') as DatabaseType,
+  port: configService.get<number>('DATABASE_PORT'),
+  username: configService.get<string>('DATABASE_USERNAME'),
+  password: configService.get<string>('DATABASE_PASSWORD'),
+  database: configService.get<string>('DATABASE_NAME'),
+  entities: ['dist/modules/**/*.entity{.ts,.js}'],
+  migrations: ['dist/lib/database/migrations/*{.ts,.js}'],
+  autoLoadEntities: true,
+  synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'),
+  logging: true,
+};
+
+export default new DataSource(config as DataSourceOptions);
+
+export const dataSourceConfig: TypeOrmModuleAsyncOptions = {
+  inject: [],
+  useFactory: async () => config,
+};
