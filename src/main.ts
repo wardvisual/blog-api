5a176845f7cd600ad96caa077a61670541946e9b
wardvisual <wardvisual@gmail.com>
2023-09-21 23:07:35 +0800

    chore: setup swagger doc

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import * as express from 'express';
 
 import { configService } from '@/lib/services/env.service';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
+
   app.setGlobalPrefix('api/v1');
+
+  setupSwagger(app);
 
   app.use('/public/assets', express.static('public/assets'));
 
