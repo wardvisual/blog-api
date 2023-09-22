aca1e9a7b55f93bd583bf8c85070823785802891
wardvisual <wardvisual@gmail.com>
2023-09-22 23:31:33 +0800

    doc: add swagger decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,19 +5,27 @@ import { BaseController } from '@/lib/controllers/base.controller';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('authentication')
 export class AuthController extends BaseController {
   constructor(private readonly authService: AuthService) {
     super();
   }
 
+  @ApiResponse({ status: 201, description: 'Successful Login' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Public()
   @Post('login')
   signIn(@Body() user: LoginDto) {
     return this.authService.signIn(user);
   }
 
+  @ApiResponse({ status: 201, description: 'Successful Registration' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   @Public()
   @Post('register')
   signUp(@Body() user: RegisterDto) {
